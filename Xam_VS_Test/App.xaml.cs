ffe0f88429aa290101b396b6042212a66989333b
JINO SHAJI <jinoshajiv@gmail.com>
2020-06-07 22:14:10 +0530

    Add project files.

---
+++ Xam_VS_Test/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xam_VS_Test.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xam_VS_Test
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Xam_VS_Test/Views/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xam_VS_Test.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        Frame frameSelected;
+        public MainPage()
+        {
+            InitializeComponent();
+            VisualStateManager.GoToState(AllNewsTab, "Selected");
+            frameSelected = AllNewsTab;
+        }
+        protected override void OnAppearing()
+        {
+            VisualStateManager.GoToState(AllNewsTab, "Selected");
+            base.OnAppearing();
+        }
+        private void Tab_Tapped(object sender, EventArgs e)
+        {
+            if (frameSelected != null)
+                VisualStateManager.GoToState(frameSelected, "UnSelected");
+
+            VisualStateManager.GoToState((Frame)sender, "Selected");
+
+            frameSelected = (Frame)sender;
+        }
+    }
+}
